c88b6ed932433f397fb9156c5945ffbd17cc62f1
Jace Mouse <jacemouse@jacemouse.com>
2013-07-25 15:22:12 -0500

    intiial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsmService::Application.config.secret_token = '98defbbb4759832e8b94156ca54e0fbe5a51ccacbab908e19e4eecd7e47c5a07c604c820bf060c10d9718806255fec2a50214d3c90a05849cddbf75374793035'
